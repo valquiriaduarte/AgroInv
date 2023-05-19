3c0b46ebce7c84f15f0e1c019ee51c5f67317f06
Mexuz <controleryan@gmail.com>
2023-05-19 17:02:56 -0300

    Remoção de uma unidade de ponto

--- Apps/Controller/AdminLayout/AddItemPage.php
+++ Apps/Controller/AdminLayout/AddItemPage.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . "/../../Utils/View.php";
-require_once __DIR__ . "./AdminLayout.php";
+require_once __DIR__ . "/AdminLayout.php";
 
 class AddItemPage extends AdminLayout{
   public static function get(){
